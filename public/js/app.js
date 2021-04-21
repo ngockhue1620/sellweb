482adaaddbb3314bdb938c138bf9857d3c8dceae
ngockhue1620 <ngockhuentca2k@gmial.com>
2021-04-21 18:28:19 +0700

    fix home page

--- public/js/app.js
+++ public/js/app.js
@@ -2366,18 +2366,17 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-function Example() {
+function HomePage() {
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
     className: "container",
     children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_Header__WEBPACK_IMPORTED_MODULE_2__.default, {}), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_Body__WEBPACK_IMPORTED_MODULE_3__.default, {})]
   });
 }
 
-x;
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Example);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);
 
 if (document.getElementById('example')) {
-  react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(Example, {}), document.getElementById('example'));
+  react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(HomePage, {}), document.getElementById('example'));
 }
 
 /***/ }),
