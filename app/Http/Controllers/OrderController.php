c4b1f8cacdd4aa1e1811105844479387d0ceba7d
ngockhue1620 <ngockhuentca2k@gmial.com>
2021-04-28 15:27:39 +0700

    aaa

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,7 +16,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        return Order::with(['orderDetail'])->get();
+        return Order::all();
     }
 
     /**
