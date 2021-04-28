0fd8d4c8b1f55fb5660ace0fcce5cd0fce09747f
ngockhue1620 <ngockhuentca2k@gmial.com>
2021-04-28 10:10:42 +0700

    indentify pgsql

--- database/migrations/2021_04_21_013711_create_customers_table.php
+++ database/migrations/2021_04_21_013711_create_customers_table.php
@@ -14,7 +14,7 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->bigIncrements("id");
+            $table->bigIncrements("id")->generatedAs()->always();
             $table->string("customerName");
             $table->string("phone");
             $table->string("email");
