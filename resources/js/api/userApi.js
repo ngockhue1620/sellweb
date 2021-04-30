f43e614a2ccb1f598303d5d8eb165f768043c5df
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-04-30 10:24:15 +0700

    update login by real api

--- resources/js/api/userApi.js
+++ resources/js/api/userApi.js
@@ -3,8 +3,8 @@ import axiosClient from './axiosClient'
 const userApi = {
     login: (data) => {
      
-      const url = 'api/customer/create';
-      return axiosClient.get(url,data);
+      const url = 'api/login';
+      return axiosClient.post(url,data);
     },
   
    
