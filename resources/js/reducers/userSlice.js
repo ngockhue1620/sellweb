d9248bf4c153d77167305d2eef8f5b8816394c6e
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-05-09 17:23:47 +0700

    add login with gg

--- resources/js/reducers/userSlice.js
+++ resources/js/reducers/userSlice.js
@@ -5,9 +5,8 @@ export const login = createAsyncThunk(
     "user/login",
     async (data, params, thunkAPI) => {
         const user = await userApi.login(data);
-        if (user.length > 0) 
-        {
-            console.log("user",user)
+        if (user.length > 0) {
+            console.log("user", user);
             return user[0];
         }
         return null;
@@ -17,7 +16,12 @@ export const login = createAsyncThunk(
 const userSlice = createSlice({
     name: "user",
     initialState: null,
-    reducers: {},
+    reducers: {
+        setUser: (state, action) => {
+            // const newPhoto = action.payload;
+            return action.payload;
+        },
+    },
     extraReducers: {
         [login.fulfilled]: (state, action) => {
             return action.payload;
@@ -26,5 +30,5 @@ const userSlice = createSlice({
 });
 
 const { reducer: userReducer, actions } = userSlice;
-
+export const { setUser } = actions;
 export default userReducer;
