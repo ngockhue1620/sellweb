d8b02bcd9401b51aa704517a6d76b50ba1605a90
ngockhue1620 <ngockhuentca2k@gmial.com>
2021-05-22 14:41:05 +0700

    update imgae

--- resources/js/pages/About/About.js
+++ resources/js/pages/About/About.js
@@ -94,7 +94,7 @@ export default class About extends Component {
                         </p>
                     </div>
                     <div className="ImageTeam">
-                        <img src="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.15752-9/175730374_781907649177721_7950261944294065524_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=ae9488&_nc_ohc=4Zixj2Ql62UAX8By_nx&tn=XWTZH-DLU9CnmfrH&_nc_ht=scontent-hkg4-2.xx&oh=8594c2e031e67c8fde54b0866d048510&oe=60A89614"></img>
+                        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p206x206/185745469_214213353641136_232124967443203961_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=58c789&_nc_ohc=KfBZHKCy7U4AX87hdjC&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&tp=6&oh=27164074f41b5aeb76211703e241fa2b&oe=60CFFED8"></img>
                     </div>
                 </div>
             </div>
